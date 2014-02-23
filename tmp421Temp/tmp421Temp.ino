92b3ad6784a0083d0478615e2657a613c9ecace6
Ben Forsyth <bforsyth@apple.com>
2014-02-22 21:49:55 -0800

    two temp sensors
    documented address configurations

--- tmp421Temp/tmp421Temp.ino
+++ tmp421Temp/tmp421Temp.ino
@@ -10,11 +10,25 @@
  *  Tested with two TMP421-Breakout
  *  Temperature Sensors from Modern Device
  *****************************/
+
+/* Address (1 = VCC, 0 = GND, X = Floating)
+ * A1 | A0 | Slave Address
+ * X  | 0  | 001 1100 (0x1C)
+ * X  | 1  | 001 1101 (0x1D)
+ * 0  | X  | 001 1110 (0x1E)
+ * 1  | X  | 001 1111 (0x1F)
+ * X  | X  | 010 1010 (0x2A)
+ * 0  | 0  | 100 1100 (0x4C)
+ * 0  | 1  | 100 1101 (0x4D)
+ * 1  | 0  | 100 1110 (0x4E)
+ * 1  | 1  | 100 1111 (0x4F)
+ */
+
 #include "Wire.h"
 #include <LibTemperature2.h>
 
 LibTemperature2 temp = LibTemperature2(0x2A);
-//LibTemperature2 temp2 = LibTemperature2(0x1C);
+LibTemperature2 temp2 = LibTemperature2(0x1C);
 
 void setup() {
   Serial.begin(57600);
@@ -23,8 +37,8 @@ void setup() {
 }
 
 void loop() {
- Serial.println(temp.GetTemperature());
-// Serial.print(" / ");
-// Serial.println(temp2.GetTemperature());
+ Serial.print(temp.GetTemperature());
+ Serial.print(" / ");
+ Serial.println(temp2.GetTemperature());
  delay(100);
 }
